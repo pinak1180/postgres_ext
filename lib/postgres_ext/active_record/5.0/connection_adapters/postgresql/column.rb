41df151669a880049d6bb21f0bc669e0579f72d6
Pinak Thakkar <pinak.0605@gmail.com>
2016-07-16 10:10:57 +0530

    Update column.rb

--- lib/postgres_ext/active_record/5.0/connection_adapters/postgresql/column.rb
+++ lib/postgres_ext/active_record/5.0/connection_adapters/postgresql/column.rb
@@ -2,7 +2,7 @@ module ActiveRecord
   module ConnectionAdapters
     class PostgreSQLColumn < Column
       def type
-        sql_type_metadata.sql_type.to_sym
+        #sql_type_metadata.sql_type.to_sym
       end
     end
   end
